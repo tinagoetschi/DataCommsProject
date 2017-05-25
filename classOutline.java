d71cbd61b12d02e16499e3f3a8f41c8537226459
Tina Götschi <tina@adacollege.org.uk>
2017-05-25 16:52:01 +0100

    Create classOutline.java
    Created the outline for a class that students will define

---
+++ classOutline.java
@@ -0,0 +1,23 @@
+public class XXXXXXX {
+    //attributes
+
+
+    //constructor
+ 
+    public XXXXXX( <arguments> ) {
+
+        //set the attributes according to arguments passed
+
+    }
+  
+    // write a method that allows a user of this class to gain some insight into attributes and methods
+    public void TellMeAboutYourself() {
+
+      
+    }
+    
+    //add methods as appropriate
+
+   
+
+} // end of  Class
