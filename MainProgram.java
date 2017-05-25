30ff041f0b54742d2e52cba395097ba07a7d9073
Tina Götschi <tina@adacollege.org.uk>
2017-05-25 16:56:01 +0100

    Create MainProgram.java
    created the outline for the class with the main method

---
+++ MainProgram.java
@@ -0,0 +1,18 @@
+//imports... you may want input from the user?
+import java.util.Scanner;
+
+
+public class MainXXXX {
+    
+    // create an instance of the Scanner class
+    public static Scanner userInput = new Scanner(System.in);
+
+    // your main program starts here... write you code to instatiate and test your class
+    public static void main(String[] args) {
+
+       
+        System.out.println("Welcome to my Data Comms Protocol Project");
+
+
+    }
+}
